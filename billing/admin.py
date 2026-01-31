fd1f031a753c0cac3c52072c61c7e91499016b39
Koyuki <prisset.noah@gmail.com>
2026-01-31 22:10:22 +0100

    admin register models

--- billing/admin.py
+++ billing/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Product, Invoice, InvoiceItem
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # Ce qu'on affiche dans la liste des produits (admin)
+    list_display = ("id", "name", "price", "expiration_date")
+
+
+class InvoiceItemInline(admin.TabularInline):
+    # Permet de voir/éditer les lignes directement dans une facture (admin)
+    model = InvoiceItem
+    extra = 0
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at")
+    inlines = [InvoiceItemInline]
+
+
+@admin.register(InvoiceItem)
+class InvoiceItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "invoice", "product", "quantity", "unit_price")
